0184e2240f21585572a14069dddc73095e6f68a7
Rafael Lima <rafaeldblima@gmail.com>
2018-05-14 22:04:37 -0400

    Added a custom user profile model, a manager, and the migrations for our database.

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from . import managers
 
 # Create your models here.
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Represents a 'user profile' inside our system."""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = managers.UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Used to get user full name."""
+        return self.name
+
+    def get_short_name(self):
+        """Used to get user short name."""
+        return self.name
+
+    def __str__(self):
+        return '{self.name} - {self.email}'
+
